e42224dbf9f0e758cf956066236131cae03e9d70
0xInfty <anirix.vall@gmail.com>
2024-09-30 11:38:07 +0100

    Installation script for Anaconda [N1]

---
+++ install.sh
@@ -0,0 +1,2 @@
+# Remember to first create a Python Anaconda environment; e.g. conda create -n pyvtools python
+conda install numpy matplotlib conda-forge::opencv scikit-image pillow scikit-learn conda-forge::tifffile
