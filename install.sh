929f501d6c7a01927d554bddfa2279de334f1ea2
0xInfty <anirix.vall@gmail.com>
2024-10-01 11:59:01 +0100

    Updated requirements & installation instructions [R1.2.0][F3][S1]
    Release:
    	new version set to 1.2.0
    
    Functional:
    	updated Python version requirement to >=3.11.0
    	also numpy version requirement to <=1.26.0, because numpy>=2.0.0 conflicts with opencv-python==4.10.0.84, raising issues with the float32 dtype
    	updated list of packages in the Anaconda environment, in case anyone wants to replicate it
    
    Syntax:
    	filled README :P

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
-# Remember to first create a Python Anaconda environment; e.g. conda create -n pyvtools python
-conda install numpy matplotlib conda-forge::opencv scikit-image pillow scikit-learn conda-forge::tifffile
+# conda create -n dev python=3.11.0; conda activate dev
+conda install python=3.11.0 numpy<=1.26.4 \
+    matplotlib conda-forge::opencv scikit-image pillow scikit-learn conda-forge::tifffile
+pip install pyvtools
+# Line1 > Anaconda environment with Python 3.11.0 to ensure compatibility
+# Line2 > Required packages = Scientific tools + Image processing libraries
+# Line3 > Current package's scientific custom tools
