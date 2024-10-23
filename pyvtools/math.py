e77cf3e9116458248e41893e98602758bc9e773b
0xInfty <anirix.vall@gmail.com>
2024-10-23 13:28:14 +0100

    Sinc interpolation for time signals [N1][S1]
    New:
    	sinc interpolation function for time signals, padding in Fourier space and transforming back to time domain
    
    Syntax:
    	just moved a function to a different spot :P

--- pyvtools/math.py
+++ pyvtools/math.py
@@ -1,10 +1,6 @@
 import numpy as np
 
-def round_and_clip(array, minimum=None, maximum=None, dtype=np.float32):
-    """Rounds up values in an array, limiting values to [min, max]"""
-    if minimum is None: minimum = np.min(array)
-    if maximum is None: maximum = np.max(array)
-    return np.clip(array.round(), minimum, maximum).astype(dtype)
+#%% NORMALIZATION
 
 def minmax_normalize(array, minimum=None, maximum=None, symmetric=False):
     if minimum is None: minimum = np.min(array)
@@ -14,3 +10,28 @@ def minmax_normalize(array, minimum=None, maximum=None, symmetric=False):
 
 def invert_minmax_normalize(array, minimum, maximum):
     return array*(maximum - minimum) + minimum
+
+#%% QUANTIZATION
+
+def round_and_clip(array, minimum=None, maximum=None, dtype=np.float32):
+    """Rounds up values in an array, limiting values to [min, max]"""
+    if minimum is None: minimum = np.min(array)
+    if maximum is None: maximum = np.max(array)
+    return np.clip(array.round(), minimum, maximum).astype(dtype)
+
+#%% INTERPOLATION
+
+def sinc_interpolation(signal, interpolation_factor):
+  """Credit to Oliver Neill, 2024"""
+
+  # Get Fourier transform
+  N = len(signal)
+  fourier = np.fft.fft(signal, norm="ortho")
+
+  # Pad Fourier transform with zeroes
+  fourier_2 = np.pad(fourier, [0, (interpolation_factor-1)*N])
+
+  # Transform back from Fourier space
+  signal_2 = np.fft.ifft(fourier_2, norm="ortho")
+  
+  return np.sqrt(interpolation_factor)*signal_2 # renormalise
